2d1a506e35f6e684012e1ce2dec403bf2e63f542
MUYEONKIM <rlaandus2@gmail.com>
2023-07-14 19:50:33 +0900

    게시판 조회 업로드

---
+++ pages/boards/index.js
@@ -0,0 +1,5 @@
+import BoardlistContainer from "../../src/components/units/board/list/Boardlist.container";
+
+export default function Boardlist() {
+  return <BoardlistContainer />
+}
---
+++ src/components/units/board/list/Boardlist.quires.js
@@ -0,0 +1,21 @@
+import { gql } from "@apollo/client";
+
+export const FetchBoards = gql`
+  query fetchBoards($page: Int) {
+    fetchBoards(page: $page) {
+      _id
+      writer
+      title
+      createdAt
+  # 특정페이지만 불러오기 할때
+  # query fetchBoards{
+  #   fetchBoards(page: 3) {
+  #     _id
+  #     writer
+  #     title
+  #     createdAt
+  #   }
+  # }
+    }
+  }
+` 
