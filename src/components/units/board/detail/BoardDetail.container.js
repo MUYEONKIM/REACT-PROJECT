e086c15ed13dc8cdee7c612a16920b69602d4bd8
MUYEONKIM <rlaandus2@gmail.com>
2023-07-16 17:52:33 +0900

    게시글 삭제 기능

--- src/components/units/board/detail/BoardDetail.container.js
+++ src/components/units/board/detail/BoardDetail.container.js
@@ -1,7 +1,7 @@
-import { useQuery } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
 import { useRouter } from "next/router";
 import BoardDetailUI from "./BoardDetail.presenter";
-import { FETCH_BOARD } from "./BoardDetail.queries";
+import { FETCH_BOARD, DELETE_BOARD } from "./BoardDetail.queries";
 
 
 export default function BoardDetailContainer() {
@@ -10,6 +10,25 @@ export default function BoardDetailContainer() {
   const { data } = useQuery(FETCH_BOARD, {
     variables: { boardId: router.query.boardid },
   });
+
+  const [deleteBoard] = useMutation(DELETE_BOARD)
+
+  const onClickMoveBoards = () => {
+    router.push("/boards")
+  }
+
+  const onClickDelete = async () => {
+    console.log('aaa')
+    try {
+      await deleteBoard({
+        variables: {boardId : router.query.boardid},
+      });
+      alert("게시글이 삭제되었습니다.")
+      router.push("/boards")
+    } catch(error) {
+      console.error(error)
+    }
+  }
   
-  return <BoardDetailUI data={data}/>
+  return <BoardDetailUI data={data} onClickDelete={onClickDelete} onClick={onClickMoveBoards} />
 }
