97e30cd79b4547ca5e236b9c5fee04c2f18c0f9f
MUYEONKIM <rlaandus2@gmail.com>
2023-11-15 15:47:22 +0900

    재로그인시 로그인권한 문제 오류 해결
    재로그인시 로그인권한 문제 오류 해결, 쿠키에 저장된 restoreAccessToken으로 접근 권한을 설정했었으나 로그아웃 시 restoreAccessToken이 없어진 후 렌더링이 안되어서 recoil에 저장된 accessToken을 사용하여 접근 권한을 설정함

--- src/components/commons/hocs/withAuth.tsx
+++ src/components/commons/hocs/withAuth.tsx
@@ -15,9 +15,7 @@ export const withAuth =
     const [accessToken] = useRecoilState(accessTokenState);
     useEffect(() => {
       void valueload.toPromise().then((newAccessToken) => {
-        console.log("withAuth가 실행됬어요");
-        // console.log(accessToken, newAccessToken);
-        if (newAccessToken === undefined) {
+        if (accessToken === "") {
           Modal.error({ content: "로그인 후 이용가능합니다." });
           void router.push("/");
         }
