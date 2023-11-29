f5c50127fc3dffc2d97f203b4917c003187c39de
MUYEONKIM <rlaandus2@gmail.com>
2023-11-29 20:10:44 +0900

    좋아요 싫어요 기능 cache 쓰도록 변경 및 optimistic ui 적용
    좋아요 싫어요 기능 cache 쓰도록 변경 및 optimistic ui 적용

--- src/components/commons/hooks/custom/useDislikeBoard.tsx
+++ src/components/commons/hooks/custom/useDislikeBoard.tsx
@@ -1,30 +1,43 @@
 import { useRouter } from "next/router";
-import { FETCH_BOARD } from "../queries/useQueryFetchBoard";
+import { FETCH_BOARD, useQueryFetchBoard } from "../queries/useQueryFetchBoard";
 import { useMutationDislikeBoard } from "../mutations/useMutationDislikeBoard";
 
-export const useDislikeBoard = (id :string) => {
+export const useDislikeBoard = (id: string) => {
   const router = useRouter();
-  const [DislikeBoard] = useMutationDislikeBoard();
-  
-  const onClickDislike = async (): Promise<void> => {
-    try {
-      if (typeof router.query.boardid !== "string") {
-        alert("시스템에 문제가 있습니다.")
-        return;
-      }
-      await DislikeBoard({
-        variables : {boardId: id},
-        refetchQueries : [
-          {
-            query : FETCH_BOARD,
-            variables : {boardId : id}
-          }
-        ]
-      })
-    } catch(error) {
-      console.log(error)
-    }
-  }
+  const [dislikeBoard] = useMutationDislikeBoard();
+  const { data } = useQueryFetchBoard({ boardId: id });
 
-  return onClickDislike
-}
+  const onClickDislike = async (): Promise<void> => {
+    if (typeof router.query.boardid !== "string") {
+      alert("시스템에 문제가 있습니다.");
+      return;
+    }
+    void dislikeBoard({
+      variables: {
+        boardId: id,
+      },
+      optimisticResponse: {
+        dislikeBoard: (data?.fetchBoard.dislikeCount ?? 0) + 1,
+      },
+      update: (cache, { data }) => {
+        // cache.modify가 있었는데 이것은 기존 값을 새로운 걸로 바꾸는 것 이었음
+        // writeQuery는 없던 것도 새롭게 추가 가능
+        cache.writeQuery({
+          query: FETCH_BOARD,
+          variables: {
+            boardId: id,
+          },
+          data: {
+            fetchBoard: {
+              _id: id, // 기준이 되기 때문에 필수
+              __typename: "Board", // 기준이 되기 때문에 필수
+              dislikeCount: data?.dislikeBoard,
+            },
+          },
+        });
+      },
+    });
+  };
+
+  return onClickDislike;
+};
--- src/components/commons/hooks/custom/useLikeBoard.tsx
+++ src/components/commons/hooks/custom/useLikeBoard.tsx
@@ -1,30 +1,42 @@
 import { useRouter } from "next/router";
 import { useMutationLikeBoard } from "../mutations/useMutationLikeBoard";
-import { FETCH_BOARD } from "../queries/useQueryFetchBoard";
+import { FETCH_BOARD, useQueryFetchBoard } from "../queries/useQueryFetchBoard";
 
-export const useLikeBoard = (id :string) => {
+export const useLikeBoard = (id: string) => {
   const router = useRouter();
-  const [LikeBoard] = useMutationLikeBoard();
-  
-  const onClickLike = async (): Promise<void> => {
-    try {
-      if (typeof router.query.boardid !== "string") {
-        alert("시스템에 문제가 있습니다.")
-        return;
-      }
-      await LikeBoard({
-        variables : {boardId: id},
-        refetchQueries : [
-          {
-            query : FETCH_BOARD,
-            variables : {boardId : id}
-          }
-        ]
-      })
-    } catch(error) {
-      console.log(error)
-    }
-  }
+  const [likeBoard] = useMutationLikeBoard();
+  const { data } = useQueryFetchBoard({ boardId: id });
 
-  return onClickLike
-}
+  const onClickLike = (): void => {
+    if (typeof router.query.boardid !== "string") {
+      alert("시스템에 문제가 있습니다.");
+      return;
+    }
+    void likeBoard({
+      variables: {
+        boardId: id,
+      },
+      optimisticResponse: {
+        likeBoard: (data?.fetchBoard.likeCount ?? 0) + 1,
+      },
+      update: (cache, { data }) => {
+        // cache.modify가 있었는데 이것은 기존 값을 새로운 걸로 바꾸는 것 이었음
+        // writeQuery는 없던 것도 새롭게 추가 가능
+        cache.writeQuery({
+          query: FETCH_BOARD,
+          variables: {
+            boardId: id,
+          },
+          data: {
+            fetchBoard: {
+              _id: id, // 기준이 되기 때문에 필수
+              __typename: "Board", // 기준이 되기 때문에 필수
+              likeCount: data?.likeBoard,
+            },
+          },
+        });
+      },
+    });
+  };
+  return onClickLike;
+};
